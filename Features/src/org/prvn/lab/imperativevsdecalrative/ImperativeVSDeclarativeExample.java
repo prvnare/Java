a234f1259dbb931cd0340adf4d3ba5d5363ce21e
Praveen Are <arewar.praveen@gmail.com>
2021-12-25 22:02:32 +0530

    diff in Imperative and Declarative programming

--- Features/src/org/prvn/lab/imperativevsdecalrative/ImperativeVSDeclarativeExample.java
+++ Features/src/org/prvn/lab/imperativevsdecalrative/ImperativeVSDeclarativeExample.java
@@ -1,10 +1,25 @@
 package org.prvn.lab.imperativevsdecalrative;
 
 import java.util.logging.Logger;
+import java.util.stream.IntStream;
+
+import static java.text.MessageFormat.format;
 
 public class ImperativeVSDeclarativeExample {
     static Logger log = Logger.getLogger("Class");
     public static void main(String[] args) {
-         log.info("Hello World");
+
+       // Imperative Programming -->  is kind "HOW" type of programming
+        int sum = 0;
+        for(int i =0;  i<=100;  i++){
+            sum += i;
+        }
+         String msgFormat = format("Sum of 100 numbers using Imperative is  : {0}", sum);
+        log.info(msgFormat);
+
+        // Declarative Programming -->  is kind "WHAT" type of programming
+        sum = IntStream.rangeClosed(1, 100).sum();
+        msgFormat = format("Sum of 100 numbers using Declarative is  : {0}", sum);
+        log.info(msgFormat);
      }
 }
